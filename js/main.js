0996a0d273a7778213011cf640c29e097a26a755
osas2211 <osaretin.frank10@gmail.com>
2020-12-30 20:10:44 +0100

    first commit. Home and About Page

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function smoothScroll () {
+    $('#main-header a').on('click', function (e){
+        if(this.hash !== ''){
+            e.preventDefault();
+
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top
+            }, 800)
+        }
+    });
+};
+
+const active = document.querySelectorAll('#main-header a');
+const aboutPhoto = document.querySelector('.about-photo');
+
+function navBarSwitch() {
+    for (let i=0; i < active.length; i++){
+        active[i].addEventListener('click', ()=>{
+            for (links of active){
+                links.classList.remove('active');
+            }
+            active[i].classList.add('active')
+
+        });
+    };
+};
+
+
+smoothScroll();
+navBarSwitch();
+if (this.hash !== 'about'){
+    active[1].addEventListener('click', ()=>{
+        aboutPhoto.style.transform = 'rotate(360deg)'
+    });
+}
